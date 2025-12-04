5f63eb9073103647f3a6a07e6a1b219d62556f61
이현진 <hjleesm@naver.com>
2025-12-04 13:01:38 +0900

    edit Variable Scope problem;

--- date-calculator.js
+++ date-calculator.js
@@ -3,20 +3,16 @@
         days: 1000 * 60 * 60 * 24
     };
     
-    var date;
     
     var Constructor = function(date_, unit_) {
-        date = new Date(date_.getFullYear(), date_.getMonth(), date_.getDate());
-        if (unit_)
-            unit = unit_;
+        this.date = new Date(date_.getFullYear(), date_.getMonth(), date_.getDate());
+        
+        if (unit_) {
+            this.unit = unit_;
+        }
     };
 
     var subDays = function(operand1, operand2) {
-        if (operand2 === undefined) {
-            operand2 = operand1;
-            operand1 = date;
-        }
-
         if (operand2 instanceof Date) {
             return Math.ceil(operand1.getTime() / DIVISORS.days) - Math.ceil(operand2.getTime() / DIVISORS.days);
         } else if (typeof operand2 === 'number') {
@@ -24,8 +20,8 @@
         }
     };
 
-    var subWeeks = function(operand) {
-        var date1 = subDays(date, parseInt(date.getDay())),
+    var subWeeks = function(currentDate, operand) {
+        var date1 = subDays(currentDate, parseInt(currentDate.getDay())),
             date2 = subDays(operand, parseInt(operand.getDay()));
 
         return subDays(date1, date2) / 7;
@@ -33,9 +29,9 @@
 
     Constructor.prototype.add = function(operand) {
         if (operand instanceof Date) {
-            date = new Date(date.getTime() + operand.getTime());
+            this.date = new Date(this.date.getTime() + operand.getTime());
         } else if (typeof operand === 'number') {
-            date = new Date(date.getTime() + (operand * DIVISORS.days));
+            this.date = new Date(this.date.getTime() + (operand * DIVISORS.days));
         }
         
         return this;
@@ -46,13 +42,13 @@
             operand = new Date(operand.getFullYear(), operand.getMonth(), operand.getDate());
 
             return {
-                days: subDays(operand),
-                weeks: subWeeks(operand),
-                months: ((date.getFullYear() - 1) * 12 + date.getMonth()) -  ((operand.getFullYear() - 1) * 12 + operand.getMonth()),
-                years: date.getFullYear() - operand.getFullYear()
+                days: subDays(this.date, operand),
+                weeks: subWeeks(this.date, operand),
+                months: ((this.date.getFullYear() - 1) * 12 + this.date.getMonth()) -  ((operand.getFullYear() - 1) * 12 + operand.getMonth()),
+                years: this.date.getFullYear() - operand.getFullYear()
             };
         } else if (typeof operand === 'number') {
-            return subDays(date, operand);
+            return subDays(this.date, operand);
         }
     };
 
